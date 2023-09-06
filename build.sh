e2c3bc043521eca19e9143878762caf0dd6677e5
Dong Hao <donghao@patsnap.com>
2023-09-06 10:52:23 +0800

    update docker file

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
-# 构建基础镜像
-docker build -f $(pwd)/build/Centos6-DNF-Base/Dockerfile -t dhso/centos:6.9 $(pwd)
-# 构建DNF服务
+# 构建CentOS6.9
+docker build -f $(pwd)/build/Centos6/Dockerfile -t dhso/centos:6.9 $(pwd)
+# 构建DNF基础镜像
+docker build -f $(pwd)/build/Centos6-DNF-Base/Dockerfile -t dhso/centos:6.9-DNF-Base $(pwd)
+# 构建DNF镜像
 docker build -f $(pwd)/build/Centos6-DNF/Dockerfile -t dhso/dnf:centos6.9 $(pwd)
